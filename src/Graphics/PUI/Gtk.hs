f776887054711f11d39d2fb6cfad3424bbec7480
Philip Kranz <pk@pmlk.net>
2017-12-11 18:55:44 +0100

    Added module Graphics.PUI.Gtk for easier import.

---
+++ src/Graphics/PUI/Gtk.hs
@@ -0,0 +1,11 @@
+module Graphics.PUI.Gtk
+  ( module Graphics.PUI.Widget
+  , module Graphics.PUI.Gtk.Image
+  , module Graphics.PUI.Gtk.Test
+  , module Graphics.PUI.Gtk.Widget
+  ) where
+
+import Graphics.PUI.Widget
+import Graphics.PUI.Gtk.Image
+import Graphics.PUI.Gtk.Test
+import Graphics.PUI.Gtk.Widget
