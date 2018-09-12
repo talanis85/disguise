7e780edfe8f4b484fd76339cdeef95cf752734af
Philip Kranz <pk@pmlk.net>
2018-09-12 19:22:37 +0200

    Add PDF module

--- test/Demo.hs
+++ test/Demo.hs
@@ -2,9 +2,14 @@ module Main where
 
 import Graphics.Disguise.Gtk.Main
 import Graphics.Disguise.Cairo
+import Graphics.Disguise.Cairo.PDF
 
 main :: IO ()
 main = do
+  putStrLn "Generating test.pdf"
+  renderPdf "test.pdf" 100 20 [ui]
+
+  putStrLn "Displaying GTK UI"
   style <- defaultStyle
   pureMain style () (const id) (const ui)
 
