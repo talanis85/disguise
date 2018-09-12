6714c288485c873055f7e2d7d1333aba2afb6bcf
Philip Kranz <pk@pmlk.net>
2018-09-12 19:23:56 +0200

    Added nix build files.

---
+++ default.nix
@@ -0,0 +1,19 @@
+{ mkDerivation, array, base, bifunctors, cairo, comonad, free, gtk3
+, JuicyPixels, ListZipper, mtl, pango, stdenv, text, vector
+}:
+mkDerivation {
+  pname = "disguise";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    array base bifunctors cairo comonad free gtk3 JuicyPixels
+    ListZipper mtl pango text vector
+  ];
+  executableHaskellDepends = [ base ];
+  testHaskellDepends = [ base ];
+  homepage = "https://github.com/talanis85/disguise#readme";
+  description = "Compositional UI on top of Gtk";
+  license = stdenv.lib.licenses.bsd3;
+}
